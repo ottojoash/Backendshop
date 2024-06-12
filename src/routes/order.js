b238a97ce40cb570c87b3532c0f2502ac31c4fba
ottojoash <louisjoshbricks@gmail.com>
2024-06-12 09:52:32 +0300

    order

--- src/routes/order.js
+++ src/routes/order.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { createOrder, getOrderHistory } = require ('../controllers/Order');
+const { createOrder, getOrderHistory } = require ('../controllers/order');
 
 // Route to create a new order
 router.post('/orders', createOrder);
