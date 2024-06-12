6c0f682e367d2e635639c40f23604651a9b0b2fc
ottojoash <louisjoshbricks@gmail.com>
2024-06-12 09:29:25 +0300

    port

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 const app = require('./src/app');
-const PORT = process.env.PORT || 3000;
 const cors = require('cors');
 
+const PORT = process.env.PORT || 3000;
 
 const corsOptions = {
   origin: 'http://localhost:5173',
 };
-// Enable CORS middleware
-app.use(cors());
-// app.use(express.json());
 
+// Enable CORS middleware with options
+app.use(cors(corsOptions));
 
+// Start the server
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
